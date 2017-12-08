4942ebbcbccea1fe64b0664cbcbaded4819170bf
TylerCN112 <tylercn112@Gmail.com>
2017-12-08 13:44:41 -0500

    Does not work

--- PortablePC/app/src/main/java/com/example/ivanchow/portablepc/ThirdFragment.java
+++ PortablePC/app/src/main/java/com/example/ivanchow/portablepc/ThirdFragment.java
@@ -3,6 +3,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
@@ -12,40 +13,44 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import static android.app.Activity.RESULT_OK;
 
 
 public class ThirdFragment extends Fragment {
+
+    ImageView uploadImage;
     EditText userInput;
     DBHandler dbHandler;
     EditText article;
-    EditText author;
+    //EditText author;
     EditText category;
     Bitmap titleImage;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_LOAD_IMAGE = 1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_third, container, false);
         dbHandler = new DBHandler(getActivity(), null, null, 8);
 
         userInput = (EditText) v.findViewById(R.id.title);
-        author = (EditText) v.findViewById(R.id.author);
+        //author = (EditText) v.findViewById(R.id.author);
         article = (EditText) v.findViewById(R.id.article);
         category = (EditText) v.findViewById(R.id.category);
+        uploadImage = (ImageView) v.findViewById(R.id.upload);
 
-        Button photo = (Button) v.findViewById(R.id.photo);
+        Button upload = (Button) v.findViewById(R.id.upload);
         Button add = (Button) v.findViewById(R.id.add);
         Button delete = (Button) v.findViewById(R.id.delete);
 
-        if(!hasCamera())
-            photo.setEnabled(false);
+       // if(!hasCamera())
+         //   upload.setEnabled(false);
 
         add.setOnClickListener(
                 new View.OnClickListener(){
                     @Override
                     public void onClick(View v) {
-                        dbHandler.addNewsStory(new NewsStory(userInput.getText().toString(), titleImage, article.getText().toString(), author.getText().toString(), category.getText().toString()));
+                        dbHandler.addNewsStory(new NewsStory(userInput.getText().toString(), titleImage, article.getText().toString(), category.getText().toString()));
 
                     }
                 }
@@ -58,11 +63,11 @@ public class ThirdFragment extends Fragment {
                     }
                 }
         );
-        photo.setOnClickListener(
+        upload.setOnClickListener(
                 new View.OnClickListener(){
                     @Override
                     public void onClick(View v) {
-                       launchCamera(v);
+                       uploadPhoto(v);
 
                     }
                 }
@@ -81,25 +86,22 @@ public class ThirdFragment extends Fragment {
 
         return f;
     }
-    //Check if the user has a camera
-    private boolean hasCamera(){
-        return getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
-    }
 
     //Launching the camera
-    public void launchCamera(View view){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    public void uploadPhoto(View view){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         //Take a picture and pass results along to onActivityResult
-        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_LOAD_IMAGE);
     }
 
     //If you want to return the image taken
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_LOAD_IMAGE && resultCode == RESULT_OK && data !=null){
             //Get the photo
-            Bundle extras = data.getExtras();
-             titleImage = (Bitmap) extras.get("data");
+            Uri selectedImage = data.getData();
+                uploadImage.setImageURI(selectedImage);
 
         }
     }
